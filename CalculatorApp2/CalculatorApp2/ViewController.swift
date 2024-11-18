a2cd9e57e97e5bfeea55f37717fd3d005c1eb9b5
minseok <pms950106@gmail.com>
2024-11-18 16:31:24 +0900

    [FEAT] 레벨 6~8 구현

--- CalculatorApp2/CalculatorApp2/ViewController.swift
+++ CalculatorApp2/CalculatorApp2/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func clickNumberButton(_ sender: UIButton) {
+        if let title = sender.titleLabel?.text {
+            label.text?.first == "0" ? label.text = title : label.text?.append(title)
+        }
+    }
+    @IBAction func clickEqualButton(_ sender: UIButton) {
+        if let result = calculate(expression: label.text ?? "0") {
+            label.text = String(result)
+        }
+        else { label.text = "잘못된 수식 입력입니다." }
+    }
+    
+    @IBAction func clickACButton(_ sender: Any) {
+        label.text = "0"
+    }
+    
+    func calculate(expression: String) -> Int? {
+        let expression = NSExpression(format: expression)
+        if let result = expression.expressionValue(with: nil, context: nil) as? Int {
+            return result
+        } else {
+            return nil
+        }
+    }
+    
 }
-
